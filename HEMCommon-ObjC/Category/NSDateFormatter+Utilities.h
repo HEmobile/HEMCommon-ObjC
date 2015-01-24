9b1fb4c4e06b9e523fd7456c3b3109b5ba848793
Marcilio Junior <marciliojrs@gmail.com>
2015-01-24 17:59:52 -0200

    added documentation

--- HEMCommon-ObjC/Category/NSDateFormatter+Utilities.h
+++ HEMCommon-ObjC/Category/NSDateFormatter+Utilities.h
@@ -10,8 +10,22 @@
 
 @interface NSDateFormatter (Utilities)
 
+/**
+ *  Returns a singleton instance of a NSDateFormatter.
+ *
+ *  @return Singleton instance of NSDateFormatter.
+ */
 + (instancetype)hem_cachedDateFormatter;
 
+/**
+ *  Returns a date representation for a given string based on a given date format.
+ *
+ *  @param string The string to parse.
+ *  @param format The format used to parse.
+ *
+ *  @return A date representation of string interpreted using the given format. If this 
+ *          method cannot parse the string, it will return nil.
+ */
 - (NSDate *)hem_dateFromString:(NSString *)string
                     withFormat:(NSString *)format;
 
--- HEMCommon-ObjC/Network/HEMBaseClient.h
+++ HEMCommon-ObjC/Network/HEMBaseClient.h
@@ -16,13 +16,48 @@ typedef NS_ENUM(NSUInteger, HEMBaseClientEnv) {
 
 @interface HEMBaseClient : AFHTTPRequestOperationManager
 
+/**
+ *  Set the base URL for production and test environment.
+ *
+ *  @param baseURLString URL string.
+ */
 + (void)setBaseURLForTestAndProduction:(NSString *)baseURLString;
+
+/**
+ *  Set the base URL for production and test environment.
+ *
+ *  @param testBaseURLString       URL String for test environment.
+ *  @param productionBaseURLString URL String for production environment.
+ */
 + (void)setBaseURLsForTest:(NSString *)testBaseURLString
                 production:(NSString *)productionBaseURLString;
+
+/**
+ *  Set the default environment for use of defaultClient method.
+ *
+ *  @param defaultEnvironment HEMBaseClientEnvTest or HEMBaseClientEnvProduction.
+ */
 + (void)setDefaultEnvironment:(HEMBaseClientEnv)defaultEnvironment;
 
+/**
+ *  Singleton instance for the a client based on default envrironment.
+ *
+ *  @return Singleton instance.
+ */
 + (instancetype)defaultClient;
+
+/**
+ *  Singleton instance for client of test environment.
+ *
+ *  @return Singleton instance.
+ */
 + (instancetype)sharedTestClient;
+
+/**
+ *  Singleton instance for client of production environment.
+ *
+ *  @return Singleton instance.
+ */
 + (instancetype)sharedProductionClient;
 
 @end
